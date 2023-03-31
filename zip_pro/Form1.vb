fc20e3c211af8603bdd1469c177529b8726019e1
Reyner Veliz <reyner@reynerveliz.com>
2023-03-31 09:01:27 -0600

    Use Using to dispose memory

--- zip_pro/Form1.vb
+++ zip_pro/Form1.vb
@@ -1,12 +1,13 @@
 ﻿Public Class Form1
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
-        Dim z As New Ionic.Zip.ZipFile()
-
-        Try
-            z.AddFile("C:\githubs\test_folder\lowpass.jpg")
-            z.Save("C:\githubs\test_folder\lowpass.zip")
-        Catch ex As Exception
-            MessageBox.Show(ex.Message)
-        End Try
+        Using z As New Ionic.Zip.ZipFile()
+            Try
+                z.AddFile("C:\githubs\test_folder\lowpass.jpg")
+                z.Save("C:\githubs\test_folder\lowpass.zip")
+                'z.Dispose()
+            Catch ex As Exception
+                MessageBox.Show(ex.Message)
+            End Try
+        End Using
     End Sub
 End Class
