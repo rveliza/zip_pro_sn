06f7ba54ba92b07d0f40fa4582ed92df814b976e
Reyner Veliz <reynerv@yahoo.com>
2023-04-09 08:12:27 -0400

    Add Multiple Files to Zip

--- zip_pro/Form1.vb
+++ zip_pro/Form1.vb
@@ -2,13 +2,17 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Using z As New Ionic.Zip.ZipFile()
             Try
-                'z.AddFile("C:\githubs\VB\lowpass.jpg")
+                'z.AddFile("C:\githubs\VB\ZipFiles\lowpass.jpg")
 
                 'Custom Folder Name
-                'z.AddFile("C:\githubs\VB\lowpass.jpg", "My_Zips")
-                z.AddFile("C:\githubs\VB\lowpass.jpg", "")
+                'z.AddFile("C:\githubs\VB\ZipFiles\lowpass.jpg", "My_Zips")
+                z.AddFile("C:\githubs\VB\ZipFiles\lowpass.jpg", "images")
+                z.AddFile("C:\githubs\VB\ZipFiles\fltplan.pdf", "aviation")
+                z.AddFile("C:\githubs\VB\ZipFiles\jet.xls", "aviation")
+                z.AddFile("C:\githubs\VB\ZipFiles\performance.pdf", "schedule")
+                z.AddFile("C:\githubs\VB\ZipFiles\tgdis.xls", "schedule")
 
-                z.Save("C:\githubs\VB\lowpass.zip")
+                z.Save("C:\githubs\VB\ZipFiles\ZippedFiles.zip")
                 'z.Dispose()
             Catch ex As Exception
                 MessageBox.Show(ex.Message)
