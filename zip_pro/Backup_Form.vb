bd331ae88e70696dd26cd719e2f28f9d80d5d056
Reyner Veliz <reynerv@yahoo.com>
2023-04-14 19:46:32 -0500

    Generate autobackup filename

--- zip_pro/Backup_Form.vb
+++ zip_pro/Backup_Form.vb
@@ -2,7 +2,12 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Me.FolderBrowserDialog1.ShowDialog()
         Dim fn As String
+        Dim file_name As String
         fn = Me.FolderBrowserDialog1.SelectedPath
-        Me.file_path_TextBox1.Text = fn
+        If fn = "" Then
+            Exit Sub
+        End If
+        file_name = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm" + ".zpr")
+        Me.file_path_TextBox1.Text = fn + "\" + file_name
     End Sub
 End Class
