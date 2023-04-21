7b5694e60c08b400074c13c7611082c6c7ba5c6a
Reyner Veliz <reynerv@yahoo.com>
2023-04-20 22:50:34 -0400

    Add empty backup file name guard before restoring

--- zip_pro/Restore_form.vb
+++ zip_pro/Restore_form.vb
@@ -12,6 +12,11 @@
     End Sub
 
     Private Sub Start_Button2_Click(sender As Object, e As EventArgs) Handles Start_Button2.Click
+        If Me.path_TextBox1.Text = "" Then
+            MessageBox.Show("Please select your file")
+            Exit Sub
+        End If
+
         Dim fn As String
         fn = Me.path_TextBox1.Text
         Using z As New Ionic.Zip.ZipFile(fn)
@@ -23,6 +28,8 @@
                 Dim df As String
                 df = Application.StartupPath
                 z.ExtractAll(df, Ionic.Zip.ExtractExistingFileAction.OverwriteSilently)
+
+                MessageBox.Show("Restoration is done! Pleae check your data!")
             Catch ex As Exception
                 MessageBox.Show(ex.Message)
             End Try
